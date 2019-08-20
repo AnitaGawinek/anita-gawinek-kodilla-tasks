bcf03e0e59f86a2b937d445e28aed41fb9ec6f34
Anita <anitagawinek@gmail.com>
2019-08-20 11:55:23 +0200

    Exercise 19.3 resolved

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 
 @RestController
 @RequestMapping("/v1/task")
